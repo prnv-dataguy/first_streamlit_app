27edae8d5d6a5ba9fdf105e7cfd0ae0478385045
prnv-dataguy <113660167+prnv-dataguy@users.noreply.github.com>
2023-03-01 14:47:18 +0530

    Update streamlit_app.py
    adding choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,3 +40,6 @@ my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
+
+add_my_fruit = streamlit.multiselect("What fruit would you like to add?", list(my_data_row))
+streamlit.text("Thanks for adding ",add_my_fruit)
