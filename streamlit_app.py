f0fcc62cc5075e566e6a62b3084fabd74bef429a
prnv-dataguy <113660167+prnv-dataguy@users.noreply.github.com>
2023-03-03 12:51:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
-import streamlit
-import pandas
-import requests
-import snowflake.connector
+#import streamlit
+#import pandas
+#import requests
+#import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title("My Parents New Healthy Diner")
 
