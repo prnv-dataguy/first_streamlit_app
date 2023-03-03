7245b766dd43dbbc3cb9586b6f74f8d844623bae
prnv-dataguy <113660167+prnv-dataguy@users.noreply.github.com>
2023-03-03 14:01:11 +0530

    Update streamlit_app.py
    bug fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,7 @@ if streamlit.button('Get Fruit Load List'):
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
     
-add_my_fruit = streamlit.multiselect("What fruit would you like to add?", list(my_data_row))
+add_my_fruit = streamlit.multiselect("What fruit would you like to add?", list(my_data_rows))
 streamlit.write(f"Thanks for adding {add_my_fruit}")
 
 my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.fruit_load_list values ('from streamlit')")
