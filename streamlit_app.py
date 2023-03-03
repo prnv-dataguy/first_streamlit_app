353939dccfb3471c02f84c45d712ee07193f266d
prnv-dataguy <113660167+prnv-dataguy@users.noreply.github.com>
2023-03-03 14:32:57 +0530

    Update streamlit_app.py
    Header change

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ try:
 except URLError as e:
   streamlit.error()
 
-streamlit.header("The fruit load list contains:")
+streamlit.header("View our Fruit List - Add your Favorites!")
 #Snowflake related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
