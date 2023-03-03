313ffae2961abed13f68e095b624d78e744d43e2
prnv-dataguy <113660167+prnv-dataguy@users.noreply.github.com>
2023-03-03 12:47:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchall()
-my_data_row = my_data_row.set_index(0)
+#my_data_row = my_data_row.set_index(0)
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 
